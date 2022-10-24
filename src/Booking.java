693c181728154cdab8a0a44e03d097053bde74a9
theblurone0329 <87411006+theblurone0329@users.noreply.github.com>
2022-10-24 12:36:28 +0800

    Edited Booking page

--- src/Booking.java
+++ src/Booking.java
@@ -1,16 +1,56 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.*;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
 
 public class Booking extends JFrame implements MouseListener{
     public static void main(String[] args) {
         new Booking();
     }
 
+    JLabel carModel = new JLabel();
+    JLabel bookingDate = new JLabel();
+    JLabel endBookingDate = new JLabel();
+    JLabel pageTitle = new JLabel();
+    JTextField txtCarModel = new JTextField();
+    JButton btnBook = new JButton();
+    JSeparator separator1 = new JSeparator();
     Booking() {
 
+        carModel.setText("Car Model");
+        carModel.setBackground(new Color(27, 28, 30));
+        carModel.setForeground(new Color(225,223,186));
+        carModel.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+        carModel.setSize(100,25);
+        carModel.setLocation(100, 70);
+
+        bookingDate.setText("Start Date");
+        bookingDate.setBackground(new Color(27, 28, 30));
+        bookingDate.setForeground(new Color(225,223,186));
+        bookingDate.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+        bookingDate.setSize(130,25);
+        bookingDate.setLocation(100, 150);
+
+        endBookingDate.setText("End Date");
+        endBookingDate.setBackground(new Color(27, 28, 30));
+        endBookingDate.setForeground(new Color(225,223,186));
+        endBookingDate.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+        endBookingDate.setSize(130,25);
+        endBookingDate.setLocation(100, 230);
+
+        //Text for Page Title
+        pageTitle.setText("Booking");
+        pageTitle.setFont(new Font("TW Cen MT", Font.PLAIN, 24));
+        pageTitle.setForeground(new Color(225,223,186));
+        pageTitle.setBackground(new Color(27, 28, 30));
+        pageTitle.setSize(122, 27);
+        pageTitle.setLocation(320, 10);
         
         ImageIcon icon = new ImageIcon("src\\Logo1.png");
         this.getContentPane().setBackground(new Color(27, 28, 30));
@@ -20,6 +60,11 @@ public class Booking extends JFrame implements MouseListener{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(725, 420);
         this.setVisible(true);
+
+        this.add(pageTitle);
+        this.add(carModel);
+        this.add(bookingDate);
+        this.add(endBookingDate);
     }
 
     @Override
